06cbdcb9d12079a97fc9dbce54c60af3eacfa7ca
SOFIA-ASIF <160203172+SOFIA-ASIF@users.noreply.github.com>
2024-03-27 18:12:06 -0700

    make a program so that you can stop the game

--- index.js
+++ index.js
@@ -4,11 +4,14 @@ let randomNumber = Math.floor(Math.random() * 10);
 let answer;
 do {
     answer = await inquirer.prompt([{ message: "select a number to guess from 1-10", type: "number", name: "userGuessNumber" }]);
-    if (answer.userGuessNumber === randomNumber) {
+    if (answer.userGuessNumber === -1) {
+        console.log("you lose the game");
+    }
+    else if (answer.userGuessNumber === randomNumber) {
         console.log('Congratulations!! You have guessed the right number');
     }
     else {
-        console.log("Try again!!");
+        console.log("Try again!! if you want to terminate the game write -1");
     }
 } while (answer.userGuessNumber !== randomNumber);
 console.log("Thank you for playing the game");
